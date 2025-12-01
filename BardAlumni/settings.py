0ea3507a52d03c6df8c4558700569651243c5fb7
Blaisehala <73476838+Blaisehala@users.noreply.github.com>
2025-12-01 18:18:31 -0500

    creatiion of user app section

--- BardAlumni/settings.py
+++ BardAlumni/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bardapp.apps.BardappConfig',
+    'users.apps. UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
